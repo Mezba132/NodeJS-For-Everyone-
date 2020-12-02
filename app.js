02e36e75eefd23a03651dc57732c932a8c7fab68
LeoMezba <csmezba@gmail.com>
2020-12-02 17:57:29 +0600

    Routing Request

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const http = require("http");
 
 const server = http.createServer( function(req, res) {
+
+    const url = req.url;
+    if( url === '/')
+    {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>')
+        res.write('<head><title>Learning Nodejs</title></head>')
+        res.write('<body></body>' +
+            '<form action="/message" method="post">' +
+            '<input type="text" name="msg"><br/><button>Post</button>' +
+            '</form>' +
+            '</body>')
+        res.write('</html>')
+        return res.end();
+    }
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>')
     res.write('<head><title>Learning Nodejs</title></head>')
