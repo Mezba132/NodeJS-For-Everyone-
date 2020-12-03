3c5ab8f8b82b84bc315381b133738a09e9f3bc7e
LeoMezba <csmezba@gmail.com>
2020-12-03 13:09:29 +0600

    Add Middleware

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const app = express();
 
-app.use( (req, res) => {
+app.use( (req, res, next) => {
+    console.log("Go To Next Middleware");
+    next();
+})
+
+app.use( (req, res, next) => {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>')
     res.write('<head><title>Learning Nodejs</title></head>')
-    res.write('<body></body><h1>Hello World</h1></body>')
+    res.write('<body></body><h1>Bye World</h1></body>')
     res.write('</html>')
     res.send();
 })
 
-app.listen(3000, function () {
+app.listen(3000,  () => {
     console.log("server is running on port 3000");
 })
