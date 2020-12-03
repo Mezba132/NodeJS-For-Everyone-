8153736c6df5b589ae29320efac03fc4160bdad1
LeoMezba <csmezba@gmail.com>
2020-12-03 14:10:07 +0600

    ParseBody,Handling Diff Route

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const app = express();
 
-app.use( (req, res, next) => {
-    console.log("Go To Next Middleware");
-    next();
+app.use( '/add-item',(req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title>Learning Nodejs</title></head>')
+    res.write('<body><form action="/item" method="post">' +
+        '<input type="text" name="msg">' +
+        '<button>Add Item</button>' +
+        '</form></body>')
+    res.write('</html>')
+    res.send();
 })
 
-app.use( (req, res, next) => {
+app.use( '/item',(req, res, next) => {
+    // parseBody
+    const body = [];
+    req.on('data', function (rawData) {
+        console.log(rawData);
+        body.push(rawData);
+    })
+    req.on('end', function() {
+        const parseBody = Buffer.concat(body).toString();
+        console.log(parseBody);
+    })
+    res.redirect('/home');
+})
+
+app.use( '/home',(req, res, next) => {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>')
     res.write('<head><title>Learning Nodejs</title></head>')
