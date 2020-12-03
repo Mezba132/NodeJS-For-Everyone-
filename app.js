fdda5be5cdf98d04dea81a015c8bb02b02a03cb9
LeoMezba <csmezba@gmail.com>
2020-12-03 18:54:11 +0600

    Add 404 page

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParsed = require("body-parser");
 const adminRoutes = require("./routes/admin");
-const userRoutes = require("./routes/user");
+const userRoutes = require("./routes/user")
+const path = require("path");
 const app = express();
 
 // 3rd Party Middleware
@@ -11,7 +12,7 @@ app.use(bodyParsed.urlencoded({extended : true}));
 app.use('/user',userRoutes);
 app.use('/admin', adminRoutes);
 app.use( (req, res, next) => {
-    res.status(404).send("<h1> Page Not Found </h1>");
+    res.status(404).sendFile(path.join(__dirname ,'views', '404.html'));
 })
 
 app.listen(3000, () => {
