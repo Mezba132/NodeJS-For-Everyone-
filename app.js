ce47a64b9e010d2a94785497a0b5b33f71095fc8
LeoMezba <csmezba@gmail.com>
2020-12-03 15:11:52 +0600

    ParseBody...Handling Diff Route

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 const express = require("express");
+const bodyParsed = require("body-parser");
 const app = express();
 
-app.use( (req, res) => {
+app.use(bodyParsed.urlencoded({extended : true}));
+
+app.use( '/add-item',(req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>')
     res.write('<head><title>Learning Nodejs</title></head>')
-    res.write('<body></body><h1>Hello World</h1></body>')
+    res.write('<body><form action="/item" method="post">' +
+        '<input type="text" name="msg">' +
+        '<button>Add Item</button>' +
+        '</form></body>')
     res.write('</html>')
     res.send();
 })
 
-app.listen(3000, function () {
+app.use( '/item',(req, res, next) => {
+    console.log(req.body);
+    res.redirect('/home');
+})
+
+app.use( '/home',(req, res, next) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title>Learning Nodejs</title></head>')
+    res.write('<body></body><h1>Bye World</h1></body>')
+    res.write('</html>')
+    res.send();
+})
+
+app.listen(3000,  () => {
     console.log("server is running on port 3000");
 })
